c6f7a887ae8ba947425a28be89be5a021366a46c
Mogasala Hemagiri <hemagiri1394.in@gmail.com>
2025-03-26 00:38:15 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer id="contact" className="py-8 bg-white dark:bg-gray-800">
       <div className="container mx-auto px-4 text-center">
-        <p className="mb-4">© {new Date().getFullYear()} [HEMAGIRI NAIDU]. All rights reserved.</p>
+        <p className="mb-4">© {new Date().getFullYear()} HEMAGIRI NAIDU. All rights reserved.</p>
         <div className="space-x-4">
           <a href="mailto:hemagiri1394.in@gmail.com.com" className="hover:text-blue-500">Email</a>
           <a href="https://linkedin.com/in/mr-hemagiri" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">LinkedIn</a>
