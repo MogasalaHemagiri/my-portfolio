69cc5dd280393faf0e5d3c9519b32b34332fe5f0
Mogasala Hemagiri <hemagiri1394.in@gmail.com>
2025-03-25 23:32:53 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// pages/index.js
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+  )
+}
