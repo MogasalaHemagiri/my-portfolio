0234fafbeab1d1d446272120dac1afab76bbb8a7
Mogasala Hemagiri <hemagiri1394.in@gmail.com>
2025-03-26 10:12:50 +0530

    Create Education.js

---
+++ components/Education.js
@@ -0,0 +1,15 @@
+const Education = () => {
+  return (
+    <section id="education" className="mb-12">
+      <h3 className="text-2xl font-semibold mb-4">🎓 Education</h3>
+      <p className="text-lg">
+        B.Tech Artificial Intelligence & Data Science -  
+        <a href="https://www.jntua.ac.in/" className="text-blue-600 font-bold hover:underline" target="_blank">
+          JNTUA University (2021 - 2025)
+        </a>
+      </p>
+    </section>
+  );
+};
+
+export default Education;
